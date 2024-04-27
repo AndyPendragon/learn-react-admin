fa9756f435e54475375ed38fe28abee4e9e25470
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-04-28 00:26:42 +0300

    refacor: add material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Admin, Resource } from 'react-admin';
 import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
 import { dataProvider } from './dataProvider';
 import { PostEdit, PostList, PostShow } from './posts';
 import { CommentEdit, CommentList, CommentShow } from './comments';
+import PostIcon from '@mui/icons-material';
+import UserIcon from '@mui/icons-material';
 
 export const App = () => (
     <Admin
         dataProvider={dataProvider}
 	>
-          <Resource name="posts" list={PostList} edit={PostEdit} show={PostShow} />
-          <Resource name="comments" list={CommentList} edit={CommentEdit} show={CommentShow} />
+      <Resource icon={PostIcon} name="posts" list={PostList} edit={PostEdit} show={PostShow} />
+      <Resource name="comments" list={CommentList} edit={CommentEdit} show={CommentShow} />
       <Resource icon={UserIcon} name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
